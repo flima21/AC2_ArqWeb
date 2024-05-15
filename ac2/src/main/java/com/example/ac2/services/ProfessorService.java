f94e39a626ff01f6f01b8494193d07bce1650ab6
Felipe Pedroso de Lima <felipe.pedroso2108@gmail.com>
2024-05-14 21:08:51 -0300

    create: classe professorservice

--- ac2/src/main/java/com/example/ac2/services/ProfessorService.java
+++ ac2/src/main/java/com/example/ac2/services/ProfessorService.java
@@ -1,5 +1,14 @@
 package com.example.ac2.services;
 
+import com.example.ac2.DTO.ProfessorDTO;
+import com.example.ac2.models.Professor;
+import java.util.List;
+
 public interface ProfessorService {
-  
+  Professor store(ProfessorDTO professor);
+
+  ProfessorDTO findById(Integer id);
+  List<ProfessorDTO> findAll();
+
+  void delete(Integer id);
 }
